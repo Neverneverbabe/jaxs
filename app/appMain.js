1f55f3b5f13fd18e2154a5b42c2aa8501a7d1d6c
Neverneverbabe <128009213+Neverneverbabe@users.noreply.github.com>
2025-06-07 21:38:37 +0000

    Final fix: track missing files for PWA app version

---
+++ app/appMain.js
@@ -0,0 +1,8 @@
+import '/jaxs/firebase.js';
+import '/jaxs/config.js';
+import '/jaxs/state.js';
+import '/jaxs/api.js';
+import '/jaxs/watchlist.js';
+import '/jaxs/handlers.js';
+import '/jaxs/ui.js';
+import '/jaxs/main.js';
