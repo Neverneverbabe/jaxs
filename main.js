02534060402c6732ed9df18567fa501c61791c5f
Neverneverbabe <128009213+Neverneverbabe@users.noreply.github.com>
2025-06-08 05:52:01 -0600

    fix: remove duplicate auth import

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // js/main.js
-import { auth } from "./firebase.js";
 import { auth, firebaseAuthFunctions, loadFirebaseIfNeeded } from './firebase.js';
 import { initApiRefs, fetchTmdbCategoryContent } from './api.js';
 import { initUiRefs, clearAllDynamicContent, showPositionSavedIndicator, positionPopup, createBackButton, clearItemDetailPanel, clearSearchResultsPanel } from './ui.js'; // Added clearItemDetailPanel, clearSearchResultsPanel
